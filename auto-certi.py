b2e0bd97827720946e2554a55a3c55cec0f5a65b
MANUJNARAYANAA <47767317+MANUJNARAYANAA@users.noreply.github.com>
2021-07-11 11:59:13 +0530

    automated certificate generation

---
+++ auto-certi.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageDraw, ImageFont
+import pandas as pd
+
+form = pd.read_excel("feedback_form.xlsx")
+name_list = form['Name'].to_list()
+for i in name_list:
+    im = Image.open("cert.jpg")
+    d = ImageDraw.Draw(im)
+    location = (418, 470)
+    text_color = (40, 35, 93)
+    font = ImageFont.truetype("courbd.ttf", 150)
+    d.text(location, i, fill=text_color,font=font)
+    im.save("certificate_"+i+".pdf")
+
+
